f2aff1a8cbd54ea848503a0385c479545992f218
CustosMagnus <mcarle@rsnweb.ch>
2022-01-24 08:49:35 +0100

    done

--- main.py
+++ main.py
@@ -22,6 +22,20 @@ def get_data(name): # extracts points from file
         txt2.append(txt[i][1])
     return txt2
 
+def fit_line(result): # linear fit
+    distance_f = []
+    distance_g = []
+    for i in range(len(result["feder_delta_s"])):
+        distance_f.append(result["feder_delta_s"][i]-result["average increment Feder"][i])
+        distance_g.append(result["gummi_delta_s"][i] - result["average increment Gummi"][i])
+    idistance_f = sum(distance_f)/len(distance_f)
+    idistance_g = sum(distance_g)/len(distance_g)
+    for i in range(len(distance_f)):
+        result["average increment Feder"][i] = result["average increment Feder"][i] + idistance_f
+        result["average increment Gummi"][i] = result["average increment Gummi"][i] + idistance_g
+
+    return result
+
 def calculate(): # main function
     feder = get_data("data_feder.txt")
     gummi = get_data("data_gummi.txt")
@@ -39,28 +53,25 @@ def calculate(): # main function
         except IndexError:
             pass
 
-    print(increment_feder)
-    print(sum(increment_feder))
     avg_incr_feder = sum(increment_feder)/len(increment_feder)
     avg_incr_gummi = sum(increment_gummi)/len(increment_gummi)
-    print(avg_incr_gummi)
 
     for i in range(10):
         result["average increment Feder"].append(avg_incr_feder*i)
         result["average increment Gummi"].append(avg_incr_gummi*i)
 
+    for i in range(10): # accuracy of best fit line
+        result = fit_line(result)
 
     result = pandas.DataFrame(result)
 
     fig = px.scatter({"gewicht": [], "strecke": []}, x="gewicht", y="strecke")
     fig.add_scatter(x=result["gewicht"], y = result["feder_delta_s"], name = "Feder")
     fig.add_scatter(x=result["gewicht"], y = result["gummi_delta_s"], name = "Gummi")
-    fig.add_scatter(x=result["gewicht"], y=result["average increment Gummi"], name="average Gummi increment")
-    fig.add_scatter(x=result["gewicht"], y=result["average increment Feder"], name="average Feder increment")
-
+    fig.add_scatter(x=result["gewicht"], y=result["average increment Gummi"], name="linear fit Gummi")
+    fig.add_scatter(x=result["gewicht"], y=result["average increment Feder"], name="linear fit Feder")
 
     fig.show()
-
     return result
 
 if __name__ == '__main__':
